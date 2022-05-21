c5ae8f419f2501ac81d99a86bdbff81673e62ec0
alfajri23 <123190088@student.upnyk.ac.id>
2022-05-21 11:47:24 +0700

    home 50%

--- resources/views/pages/public/home.blade.php
+++ resources/views/pages/public/home.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-xxxl-7 col-xl-8 vh-md-100 pt-5 pt-sm-7 pb-2 pt-sm-7 align-items-center d-flex">
                     <div class="card w-100 border-0 bg-transparent">
                         
-                        <h2 class="fw-700 text-grey-900 lh-3 display3-size display2-md-size lh-1 mb-0 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500">
+                        <h2 class="fw-700 text-grey-900 lh-3 display3-size display4-md-size lh-1 mb-0 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500">
                             Cuma Disini Kamu Diajarin<br>
                             <span class="text-current">Cara Bikin CV Terbaik</span><br>
                             Langsung Sama HRD
@@ -30,7 +30,7 @@
                         
                     </div>
                 </div>
-                <div class="col-xxxl-5 col-xl-4 vh-md-100 align-items-center d-flex home-phone">
+                <div class="col-xxxl-5 col-xl-4 vh-md-100 align-items-center justify-content-center d-flex home-phone">
                     <div class="card w-75 border-0 bg-transparent text-center d-block">
                         <img src="{{asset('asset/img/program/phone.png')}}" alt="app-bg" class="w-100 d-lg-block" >    
                     </div>
@@ -44,7 +44,7 @@
             <div class="row justify-content-center">
                 <div class="page-title style1 col-xl-6 col-lg-8 col-md-10 text-center mb-5">
                     <span class="font-xsssss fw-700 pl-3 pr-3 lh-32 text-uppercase rounded-xl ls-2 alert-warning d-inline-block text-warning mr-1">Feedback</span>
-                    <h2 class="text-grey-900 fw-700 display1-size font-md-xs pb-3 mb-0 mt-3 d-block lh-3">Makin Mahir Platform Terbaik Untuk Persiapan Kerja Kamu</h2>
+                    <h2 class="text-grey-900 fw-700 display1-size display1-sm-size pb-3 mb-0 mt-3 d-block lh-3">Makin Mahir Platform Terbaik Untuk Persiapan Kerja Kamu</h2>
                 </div>
                 <div class="col-xl-10 col-lg-12 pb-lg--5">
                     <div class="row justify-content-center">
@@ -74,7 +74,7 @@
         <div class="row justify-content-center">
             <div class="page-title style1 col-xl-6 col-lg-8 col-md-10 text-center mb-5">
                 <span class="font-xsssss fw-700 pl-3 pr-3 lh-32 text-uppercase rounded-xl ls-2 alert-warning d-inline-block text-warning mr-1">Feedback</span>
-                <h2 class="text-grey-900 fw-700 display1-size font-md-xs pb-3 mb-0 mt-3 d-block lh-3">Yang Kamu Dapatkan Di Makin Mahir</h2>
+                <h2 class="text-grey-900 fw-700 display1-size display1-sm-size pb-3 mb-0 mt-3 d-block lh-3">Yang Kamu Dapatkan<br> Di Makin Mahir</h2>
             </div>
         </div>
         <div class="container">
@@ -82,48 +82,48 @@
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="500">
                         <i class="feather-layers text-success font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">Event Gratis</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Daftar Event Bersertifikat Dengan Pemateri Profesional di Dunia Kerja</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">Event Gratis</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Daftar Event Bersertifikat Dengan Pemateri Profesional di Dunia Kerja</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="500">
                         <i class="feather-award text-danger font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">Kelas Online</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Kelas Online Dari Mulai Belajar Soft Skill Sampai Hardskill</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">Kelas Online</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Kelas Online Dari Mulai Belajar Soft Skill Sampai Hardskill</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="500">
                         <i class="feather-cpu text-info font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">Konsultasi HRD</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Konsultasi Langsung Dengan HRD Buat Bedah CV, Interview Kerja & Psikotes</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">Konsultasi HRD</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Konsultasi Langsung Dengan HRD Buat Bedah CV, Interview Kerja & Psikotes</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="500">
                         <i class="feather-hard-drive text-warning font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">CV Maker</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Buat CV ATS Kamu Langsung Online, GRATIS</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">CV Maker</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Buat CV ATS Kamu Langsung Online, GRATIS</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="400" data-aos-duration="500">
                         <i class="feather-lock text-secondary font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">CV Checker</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Cek Dulu CV Kamu Udah Menarik Belum di Mata HRD</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">CV Checker</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Cek Dulu CV Kamu Udah Menarik Belum di Mata HRD</p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 col-md-6">
                     <div class="card mb-4 w-100 border-0 pt-4 pb-4 pr-4 pl-7 shadow-xss rounded-lg aos-init" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="500">
                         <i class="feather-globe text-success font-xl position-absolute left-15 ml-2"></i>
-                        <h2 class="fw-700 font-xss text-grey-900 mt-1">Assesment Test</h2>
-                        <p class="fw-500 font-xssss lh-24 text-grey-500 mb-0">Ikuti Test Assesment Buat Tau Potensi, Minat dan Bakat Kamu</p>
+                        <h2 class="fw-700 font-xs text-grey-900 mt-1">Assesment Test</h2>
+                        <p class="fw-500 font-xssss lh-24 text-grey-600 mb-0">Ikuti Test Assesment Buat Tau Potensi, Minat dan Bakat Kamu</p>
                     </div>
                 </div>
             </div>
@@ -134,10 +134,45 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="page-title style1 col-xl-6 col-lg-8 col-md-10 text-center mb-5">
-                    <h2 class="text-grey-900 fw-700 font-xxl pb-3 mb-0 mt-3 d-block lh-3">Tumbuh Bareng<br>Mahir Bareng</h2>
-                    <p class="fw-300 font-xssss lh-28 text-grey-500">Inilah cara kami membantu Teman Cari Kerja untuk mempersiapkan diri memasuki dunia kerja</p>
+                    <h2 class="text-grey-900 fw-700 display1-size display1-sm-size pb-3 mb-0 mt-3 d-block lh-3">Tumbuh Bareng<br>Mahir Bareng</h2>
+                    <p class="fw-300 font-xss lh-28 text-grey-600">Inilah cara kami membantu Teman Cari Kerja untuk mempersiapkan diri memasuki dunia kerja</p>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="feature-wrapper layer-after">
+        <div class="container">
+              <div class="row">
+                <div class="col-lg-6">
+                    <img src="{{asset('asset/img/program/zoom_beduk.png')}}" alt="image" class="img-fluid aos-init" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="500">
+                </div>
+                <div class="col-lg-4 offset-lg-1">
+                    <span class="font-xsssss fw-700 pl-3 pr-3 lh-32 text-uppercase rounded-xl ls-2 alert-success d-inline-block text-success mr-1">Beduk</span>
+                    <h2 class="text-grey-900 fw-700 display1-size pb-3 mb-0 mt-3 d-block lh-3 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500">Live Event Bedah Dunia Kerja (BEDUK)</h2>
+                    <p class="fw-400 font-xss lh-28 text-grey-600 aos-init" data-aos="fade-up" data-aos-delay="300" data-aos-duration="500">Kami Mempertemukan Kamu Dengan HRD Melalui Live Event Rutin Setiap Pekan</p>
+                    <a href="#" class="btn border-0 bg-primary p-3 text-white fw-600 rounded-lg d-inline-block font-xssss btn-light mt-3 w150 aos-init" data-aos="fade-up" data-aos-delay="400" data-aos-duration="500">Subscribe</a>
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <div class="feature-wrapper layer-after pt-lg--7 pt-5 mt-4">
+        <div class="container">
+              <div class="row">
+
+                <div class="col-lg-7 order-lg-2 offset-lg-1">
+                    <img src="{{asset('asset/img/program/zoom_konsul.jpeg')}}" alt="image" class="w-100 aos-init" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="500">
+                </div>
+                
+                <div class="col-lg-4 order-lg-1">
+                    <span class="font-xsssss fw-700 pl-3 pr-3 lh-32 text-uppercase rounded-xl ls-2 alert-success d-inline-block text-success mr-1">Konsultasi</span>
+                    <h2 class="text-grey-900 fw-700 display1-size pb-3 mb-0 mt-3 d-block lh-3 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="500">Konsultasi Private Dengan HRD</h2>
+                    <p class="fw-400 font-xss lh-28 text-grey-600 aos-init" data-aos="fade-up" data-aos-delay="300" data-aos-duration="500">Kamu Bisa Ngobrol Langsung Sama HRD Buat Bedah CV, Latihan Interview Kerja dan Psikotes</p>
+                    <a href="#" class="btn border-0 bg-primary p-3 text-white fw-600 rounded-lg d-inline-block font-xssss btn-light mt-3 w150 aos-init" data-aos="fade-up" data-aos-delay="400" data-aos-duration="500">Subscribe</a>
+                </div>
+                
+              </div>
         </div>
     </div>
    
