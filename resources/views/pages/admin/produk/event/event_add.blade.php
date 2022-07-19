8950eac0230ed8b2ec2093fead86c9281ff09272
alfajri23 <123190088@student.upnyk.ac.id>
2022-07-19 11:05:55 +0700

    add foto

--- resources/views/pages/admin/produk/event/event_add.blade.php
+++ resources/views/pages/admin/produk/event/event_add.blade.php
@@ -106,7 +106,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script type="text/javascript">
 	$(document).ready(function() {
-		 tinymce.init({
+		tinymce.init({
 	            selector: "textarea",
 	            branding: false,
 	            width: "100%",
@@ -114,9 +114,51 @@ crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	            plugins: [
 	                "advlist autolink lists charmap print preview anchor",
 	                "searchreplace visualblocks code fullscreen",
-	                "paste wordcount link","directionality","media"
+	                "paste wordcount link","directionality",
+                    "media image code"
 	            ],
-	            toolbar: "link | undo redo | bold italic | bullist numlist outdent indent | ltr rtl"
+	            toolbar: "link | undo redo | bold italic | bullist numlist outdent indent | ltr rtl | media image",
+                image_title: true,
+                /* enable automatic uploads of images represented by blob or data URIs*/
+                automatic_uploads: true,
+                file_picker_callback: function (cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+
+                /*
+                Note: In modern browsers input[type="file"] is functional without
+                even adding it to the DOM, but that might not be the case in some older
+                or quirky browsers like IE, so you might want to add it to the DOM
+                just in case, and visually hide it. And do not forget do remove it
+                once you do not need it anymore.
+                */
+
+                input.onchange = function () {
+                var file = this.files[0];
+
+                var reader = new FileReader();
+                reader.onload = function () {
+                    /*
+                    Note: Now we need to register the blob in TinyMCEs image blob
+                    registry. In the next release this part hopefully won't be
+                    necessary, as we are looking to handle it internally.
+                    */
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
+
+                    /* call the callback and populate the Title field with the file name */
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
+                reader.readAsDataURL(file);
+                };
+
+                input.click();
+            },
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 	      
 	    });
 	});
