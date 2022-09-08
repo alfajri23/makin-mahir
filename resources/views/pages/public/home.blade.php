18e25e754a501574bfac804ff1570aea7cc90c87
alfajri23 <123190088@student.upnyk.ac.id>
2022-09-08 16:01:40 +0700

    add testi

--- resources/views/pages/public/home.blade.php
+++ resources/views/pages/public/home.blade.php
@@ -360,19 +360,113 @@
 
             <div class="row justify-content-center">
                 <div class="loop owl-carousel owl-theme owl-loaded owl-drag">
-                    @for ($i=0;$i<5;$i++)
-                        
+                       
                     <div class="owl-items text-center my-5">
                         <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
                             <div class="card-body pl-0 pt-0">
-                                <img src="https://via.placeholder.com/50x50.png" alt="user" class="w45 float-left mr-3">
-                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Goria {{$i}}</h4>    
-                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Digital Marketing Executive</h5>
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Ibnu Hari Winartomo</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Universitas Budi Luhur</h5>
                             </div>
-                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">Human coronaviruses are common and are typically associated with mild illnesses, similar to the common cold.</p>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">
+                                “ Setelah mengikuti kelas konsul ini saya jadi lebih paham cara membuat CV untuk membuat personal branding lebih menonjol dibandingkan kandidat lain. Selain itu masukan dari kak Aldino terkait portofolio sangat membantu saya.”
+                            </p>
                         </div>
                     </div>
-                    @endfor
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Dalil AF</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">ISI Denpasar, Bali</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">
+                                “Insighfull banget yah, jadi pikiran yang sebelumnya menurut saya sudah benar ternyata menurut HR ini kurang tepat. Jadi kelas bedah CV ini sangat membantu saya”
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Veronica Yolanda</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">UII Yogyakarta</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Saya jadi paham terkait pembuatan CV, baik yang ATS ataupun CV Kreatif. Terimakasih Makin Mahir”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Enzy Diwantama</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Universitas Padjajaran</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Sangat membantu banget kak, dulu saat melakukan proses interview jawabnya sesuai dengan pemikiran saya saja, ternyata pemikirian saya gak semuanya itu benar. Dan setelah mengikuti arahan kelas private ini sangat membantu saya kedepannya”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Amelia Nofiani</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Universitas Pekalongan</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Sangat bermanfaat kak menurut saya, dari program ini akhirnya saya bisa dapat ilmu tentang membuat cv yang benar, cara mengatasi agar tdk grogi saat interview dll.. semoga dengan program ini saya bisa mengubah diri saya supaya menjadi lebih baik lagi karena selama saya melamar pekerjaan kendalanya jika setelah interview terkadang tdk ada kabar lagi kak”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Arianto Sutomo</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Institute Pertanian Bogor , 25+ IT Infrastucture</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Event di Makin Mahir sangat bermanfaat, bisa di ikuti dari mana pun, mudah di pahami dan menarik topik- topiknya.”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Deni Andrean</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Politeknik Negeri Lampung , Analis Labolatorium</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Intinya sejak selesai ikut seminar dulu itu dan banyak masukan di bedah cv, atas dasar kritik di bedah cv dulu akhirnya saya rombaklah itu cv, ada perbedaan antara sebelum & sesudah dirombak, salah satunya perbandingan antara kirim lamaran kerja dengan dipanggil wawancara cukup meningkat”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Corina</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500"></h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Untuk materi yg diberikan oleh makin mahir sudah sesuai dan menjawab permasalahan dari para fresh graduate mengenai cv yg tepat utk diajukan saat melamar pekerjaan. pemateri yg membawakan materi jg handal dan terlihat sangat mendalami materi serta memiliki pembawaan yg baik sehingga mudah dimengerti”</p>
+                        </div>
+                    </div>
+
+                    <div class="owl-items text-center my-5">
+                        <div class="card w-100 p-4 p-md--5 text-left border shadow-xs rounded-lg">
+                            <div class="card-body pl-0 pt-0">
+                    
+                                <h4 class="text-grey-900 fw-700 font-xsss mt-0 pt-1">Ericha</h4>    
+                                <h5 class="font-xssss fw-500 mb-1 text-grey-500">Universitas Semarang</h5>
+                            </div>
+                            <p class="font-xsss fw-400 text-grey-500 lh-28 mt-0 mb-0 ">“Saya sangat terbantu sekali dengan adanya Webinar kemarin kak , saya lebih bisa memahami bagaimana perkembangan antr CV ats maupun kreatif ..Dan juga sangat membantu dalam melihat yg di perlukan di perusahaan menengah, atas dll .
+
+                                Terimakasih telah menyelenggarakan kegiatan webinar ini, mohon selanjutnya diadakan kembali webinar spt ini entah membahas tntg interview atau tes yg lain yg berkaitan dg rekrutmen kerja untuk menambah wawasan
+                                ”</p>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
 
